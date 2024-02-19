9f8220ddd199938e59036dea99ef9e669b23dcf5
paulaandrezza <paulaandrezza25@gmail.com>
2024-02-19 18:15:48 -0300

    Add project files.

---
+++ CurriculoApi/Controllers/CurriculoController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CurriculoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurriculoController : Controller
+    {
+        private readonly IContentTypeProvider _contentTypeProvider;
+
+        public CurriculoController(IContentTypeProvider contentTypeProvider)
+        {
+            _contentTypeProvider = contentTypeProvider;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCurriculo(IFormFile curriculo)
+        {
+            var caminho = $"wwwroot/{curriculo.FileName}";
+
+            if (ValidateFile(curriculo) != null)
+            {
+                return ValidateFile(curriculo);
+            }
+
+            using (var stream = System.IO.File.Create(caminho))
+            {
+                await curriculo.CopyToAsync(stream);
+            }
+
+            return Ok(new { Message = "Curriculo enviado com sucesso!" });
+        }
+
+        [HttpGet]
+        public IActionResult GetCurriculo(string nomeArquivo)
+        {
+            var caminho = $"wwwroot/{nomeArquivo}";
+            if (!System.IO.File.Exists(caminho))
+            {
+                return BadRequest(new { Message = $"Arquivo não existe: {nomeArquivo}" });
+            }
+
+            var fileInBytes = System.IO.File.ReadAllBytes(caminho);
+
+            var contentType = "application/octet-stream";
+            if (_contentTypeProvider.TryGetContentType(caminho, out var contentTypeProvided))
+            {
+                contentType = contentTypeProvided;
+            }
+
+            return File(fileInBytes, contentType);
+        }
+
+        private IActionResult? ValidateFile(IFormFile curriculo)
+        {
+            if (curriculo.Length > 5 * 1024 * 1024)
+            {
+                return BadRequest(new { ErrorMessage = "Tamanho excede o limite permitido de 5MB" });
+            }
+
+            string[] extensoesPermitidas = [".pdf", ".doc", ".docx"];
+
+            var extensaoArquivo = Path.GetExtension(curriculo.FileName);
+            if (!extensoesPermitidas.Contains(extensaoArquivo))
+            {
+                return BadRequest(new { ErrorMessage = "Extensão de arquivo inválida. Por favor envie um arquivo em pdf, doc ou docx" });
+            }
+
+            return null;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# API de Gerenciamento de Currículos
+
+Esta API permite que os usuários realizem o upload e download de currículos nos formatos PDF, DOC e DOCX. Ela foi desenvolvida como parte do curso diversedev da Ada tech, com o objetivo de treinar habilidades em desenvolvimento web com ASP.NET Core.
+
+## Funcionalidades
+
+- **Upload de Currículo**: Os usuários podem enviar seus currículos para a API.
+- **Download de Currículo**: Os usuários podem baixar os currículos previamente enviados.
+
+## Validações
+
+- **Tamanho do Arquivo**: O tamanho máximo permitido para um currículo é de 5MB.
+- **Extensão do Arquivo**: A API aceita apenas arquivos nos formatos PDF, DOC e DOCX.
+
+## Contribuidores
+
+Este projeto foi desenvolvido com a colaboração de:
+
+- [Charles Morais](https://github.com/charles-serafim)
+- [Edvaldo Oliveira](https://github.com/EdOliveiraJr)
+- [Paula Marinho](https://github.com/paulaandrezza)
+- [Suellen Reis](https://github.com/)
